908b63f7dca7bef768ea7ef8a9c3b1bbd04098e4
koyo01xp <21kasakawa@athenian.org>
2021-01-20 22:14:49 +0000

    homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,14 @@ public class MyMain {
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
     public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
-        return false;
+        boolean check = false;
+        if(board[row][col] == true){
+            check = true;
+        }
+        else{
+            check=false;
+        }
+        return check;
     }
 
 
@@ -13,7 +19,18 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        if(direction == "down"){
+            int r = row;
+            for(int x = r;x<boatLength+1;x++){
+                board[x][col] = true;
+            }
+        }
+        else if(direction == "right"){
+            int c = col;
+            for(int y = c;y<boatLength;y++){
+                board[row][y] = true;
+            }
+        }
         return board;
     }
 
@@ -22,8 +39,25 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        boolean check = false;
+        for(int row = 0;row<words.length;row++){
+            for(int col = 0;col<words[0].length-1;col++){
+                
+                String str = words[row][col];
+                char ch = str.charAt(0);
+                String str2 = words[row][col+1];
+                char ch2 = str2.charAt(0);
+                int x = str.length();
+                int y = str2.length();
+                if(ch>ch2 || x>y){
+                    check = false;
+                }
+                else {
+                    check = true;
+                }
+            }
+        }
+        return check;
     }
 
     public static void main(String[] args) {
